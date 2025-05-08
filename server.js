8d367b6a65fa4a6298bd16d3d2fba04a38cb4260
Bisao <godofglobal@gmail.com>
2025-05-08 11:44:37 +0000

    Restored to '38fa07576105922c8629b923c67e264f6a272af4'
    Replit-Restored-To: 38fa07576105922c8629b923c67e264f6a272af4

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-const PORT = 3000;
+const PORT = 5000;
 
 app.use(express.static(__dirname));
 
